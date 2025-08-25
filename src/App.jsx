d473ff3f0779c7177320d90d785c307613024167
LalaineHines <lalainehines@gmail.com>
2025-08-24 19:38:15 -0700

    added src folder and app jsx file

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Builder from "./components/builder/Builder";
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+import React from "react";
+
+const App = () => {
+
+    return (
+        <>
+        <Header />
+        <Builder />
+        <Footer />
+        </>
+    );
+};
+
+export default App;
